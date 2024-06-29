d4133cfd41b6cd251e82b154078d48eccddc8918
NJWeb.Solutions <dibend8@gmail.com>
2024-06-29 09:52:50 -0400

    pwa

--- service-worker.js
+++ service-worker.js
@@ -4,13 +4,24 @@ const assetsToCache = [
   '/index.html',
   '/styles.css',
   '/script.js',
-  '/icons/icon-192x192.png'  // Add the icon to the assets to cache
+  '/icons/icon-192x192.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName).then(cache => {
-      return cache.addAll(assetsToCache);
+      return Promise.all(
+        assetsToCache.map(url => {
+          return fetch(url).then(response => {
+            if (!response.ok) {
+              throw new Error('Request failed: ' + url);
+            }
+            return cache.put(url, response);
+          }).catch(error => {
+            console.error('Failed to cache:', url, error);
+          });
+        })
+      );
     })
   );
 });
